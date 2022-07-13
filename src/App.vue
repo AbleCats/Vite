189fe06223d9c5b3243b5650e3f6b7b3f6763239
AbleCats <etsy@live.com>
2022-07-13 18:10:51 +0800

    修改

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script setup>
 import { Layout } from 'ant-design-vue'
+import HeaderBar from '@/components/header/index.vue';
 </script>
 
 <template>
   <a-layout class="layout">
-    <a-layout-header>Header</a-layout-header>
+    <a-layout-header>
+      <HeaderBar />
+    </a-layout-header>
     <a-layout-content>
       <router-view />
     </a-layout-content>
@@ -27,9 +30,14 @@ body,
 
 .layout {
   height: 100%;
+  background-color: #fff;
 }
 
 :deep(.ant-layout-header) {
+  padding: 0;
   background: transparent;
+}
+:deep(.ant-layout-content) {
+  padding: 10px;
 }
 </style>
---
+++ src/components/header/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="header-bar">
+    <div class="icon"></div>
+    <div class="title">Cats App</div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.header-bar {
+  display: flex;
+  align-items: center;
+  border-radius: 12px;
+  box-shadow: 0 20px 27px rgb(0 0 0 / 5%);
+  .icon {
+    width: 48px;
+    height: 48px;
+    margin: auto 20px;
+    background-image: url("/favicon.ico");
+  }
+  .title {
+    flex: 1;
+    font-size: 18px;
+    font-weight: 600;
+  }
+}
+</style>
